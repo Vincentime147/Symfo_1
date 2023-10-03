07011323a0a89b794552cf3df4fff08b9105e80b
Vincentime147 <vincentime147@gmail.com>
2023-10-04 01:42:55 +0200

    Logique Twig Compris (faut pas travaillé tard comme ça...)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 //use Twig\Environment;
 
 
@@ -14,8 +16,21 @@ class HomeController extends AbstractController
 {
     //route déclaré dans le "routes.yaml"
     public function accueil() { 
-        return $this->render('Accueil.php');     
+        //return $this->render('Accueil.php');  
+        return $this->render('acceuil.html.twig', [
+            
+        ]);   
     }
+
+    #[Route('/accueil/reponse')]
+    public function accueil2(Request $request) { 
+        //return $this->render('Accueil.php');
+        $nom =  $request->request->get('nom'); 
+        return $this->render('acceuil2.html.twig', [
+            'nom' => $nom,
+        ]);   
+    }
+
 
     #[Route('/asuka')] //Redirection vers une vrai page
     public function assuka(): Response 
